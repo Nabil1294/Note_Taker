feae099b294f7e2ead1ce7b20355aef0aeec5d94
Mahmoud Nabil <mahmoudmuhamednabil@gmail.com>
2023-10-15 00:38:11 -0400

    adding routes dev codes

--- server.js
+++ server.js
@@ -12,3 +12,36 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // adding static middleware to use files in public
 app.use(express.static('public'));
+// adding routes to html pages
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+app.get('api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './db/db.json'), (err) => {
+        if(err) throw err;
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// listen() method is responsible for listening for incoming connections on the specified port
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
